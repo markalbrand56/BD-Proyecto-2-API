07e75bdc6b3479d639b6dbfdbe6d844ca3337930
Mark Albrand <mark.albrand5@gmail.com>
2023-04-15 13:09:26 -0600

    Record

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import datetime
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
 from dotenv import load_dotenv
 import os
 import psycopg2
@@ -47,9 +51,20 @@ async def get_users():
     query = 'SELECT * FROM "usuario"'
     cur.execute(query)
     rows = cur.fetchall()
+
+    result = []
+    for row in rows:
+        result.append({
+            "dpi": row[0],
+            "rol": row[1],
+            "contrasena": row[2]
+        })
+    users = jsonable_encoder(result)
+
     cur.close()
     conn.close()
-    return rows
+    return JSONResponse(content=users)
+
 
 @app.get("/user/login/")
 async def login_user(user: models.UserLogin):
@@ -80,5 +95,53 @@ async def login_user(user: models.UserLogin):
                 "direccion": row[2],
                 "telefono": row[3],
                 "num_colegiado": row[4],
-                "especialidad": row[5]
+                "especialidad": row[5],
+                "unidad_de_salud": None,  # CAMBIAR
             }
+
+
+@app.post("/user/signup/")
+async def signup_user(user: models.UserSignIn):
+    conn = connect_db()
+    cur = conn.cursor()
+    query_usuario = f"INSERT INTO usuario VALUES ('{user.dpi}', '{user.rol}', '{user.password}')"
+    query_medico = f"INSERT INTO medico VALUES ('{user.dpi}', '{user.nombre}', '{user.direccion}', '{user.telefono}', '{user.num_colegiado}', '{user.especialidad}')"
+    query_trabaja = f"INSERT INTO trabaja VALUES ('{datetime.date.today()}', null, '{user.dpi}', '{user.unidad_de_salud}')"
+    print(query_usuario)
+    print(query_medico)
+    print(query_trabaja)
+    return {"message": "User created successfully"}
+
+
+@app.get("/record/")
+async def get_records(id: models.RecordSearch):
+    conn = connect_db()
+    cur = conn.cursor()
+    query = f"SELECT * FROM expediente WHERE paciente_dpi = '{id.dpi}'"
+    cur.execute(query)
+    rows = cur.fetchall()
+
+    if rows is None or len(rows) == 0:
+        cur.close()
+        conn.close()
+        return {"message": "No records found"}
+
+    result = []
+    for row in rows:
+        result.append({
+            "no_expediente": row[0],
+            "paciente_dpi": row[1],
+            "medico_encargado": row[2],
+            "enfermedad_id": row[3],
+            "examenes": row[4],
+            "diagnosticos": row[5],
+            "fecha_atencion": row[6],
+            "cirugias": row[7],
+            "status": row[8],
+            "unidad_salud_id": row[9]
+        })
+
+    expedientes = jsonable_encoder(result)
+    cur.close()
+    conn.close()
+    return JSONResponse(content=expedientes)
--- models.py
+++ models.py
@@ -1,15 +1,28 @@
 from pydantic import BaseModel
 
 
+class User(BaseModel):
+    dpi: str
+    rol: str
+    password: str
+
+
 class UserLogin(BaseModel):
     dpi: str
     password: str
 
 
-class User(BaseModel):
+class UserSignIn(BaseModel):
     dpi: str
     nombre: str
     direccion: str
     telefono: str
     num_colegiado: str
     especialidad: str
+    unidad_de_salud: int
+    rol: str
+    password: str
+
+
+class RecordSearch(BaseModel):
+    dpi: str
